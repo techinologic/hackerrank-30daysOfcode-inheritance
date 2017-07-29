9fd3567dee2422f6e29b208fcfe170f3aa3256d6
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-07-29 11:12:16 -0400

    code working.

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Person {
     protected String firstName;
@@ -6,21 +6,21 @@ class Person {
     protected int idNumber;
 
     // Constructor
-    Person(String firstName, String lastName, int identification){
+    Person(String firstName, String lastName, int identification) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNumber = identification;
     }
 
     // Print person data
-    public void printPerson(){
+    public void printPerson() {
         System.out.println(
                 "Name: " + lastName + ", " + firstName
-                        + 	"\nID: " + idNumber);
+                        + "\nID: " + idNumber);
     }
-
 }
-class Student extends Person{
+
+class Student extends Person {
     private int[] testScores;
 
     /*
@@ -38,37 +38,43 @@ class Student extends Person{
         this.testScores = testScores;
     }
 
-
-
     /*
     *   Method Name: calculate
     *   @return A character denoting the grade.
     */
     // Write your method here
 
-/*
-    public static String calculate() {
-        int ave = 0;
-        String grade = "";
-        int length =
-        for (int i = 0; i < ; i++) {
 
+    String calculate() {
+        int ave;
+        int score = 0;
+        int length = getTestScores().length;
+        for (int i = 0; i < length; i++) {
+            score += getTestScores()[i];
         }
+        ave = score / getTestScores().length;
 
-        return grade;
+        if (ave >= 90 && ave <= 100) {
+            return "O";
+        } else if (ave >= 80 && ave < 90) {
+            return "E";
+        } else if (ave >= 70 && ave < 80) {
+            return "A";
+        } else if (ave >= 55 && ave < 70) {
+            return "P";
+        } else if (ave >= 40 && ave < 55) {
+            return "D";
+        } else if (ave < 40) {
+            return "T";
+        } else {
+            return "Invalid grade";
+        }
     }
-*/
 
-    public int[] getTestScores() {
+    private int[] getTestScores() {
         return testScores;
     }
-
-    public void setTestScores(int[] testScores) {
-        this.testScores = testScores;
-    }
 }
-
-
 
 class Solution {
     public static void main(String[] args) {
@@ -78,19 +84,13 @@ class Solution {
         int id = scan.nextInt();
         int numScores = scan.nextInt();
         int[] testScores = new int[numScores];
-        for(int i = 0; i < numScores; i++){
+        for (int i = 0; i < numScores; i++) {
             testScores[i] = scan.nextInt();
         }
         scan.close();
 
         Student s = new Student(firstName, lastName, id, testScores);
         s.printPerson();
-        //System.out.println("Grade: " + s.calculate() );
-
-        for (int n :
-                testScores) {
-            System.out.println(n);
-
-        }
+        System.out.println("Grade: " + s.calculate());
     }
 }
